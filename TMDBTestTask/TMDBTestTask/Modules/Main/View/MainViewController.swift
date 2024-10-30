6bbc2b1879f7cbff13b169b34981dc3bb438501f
SerhiiMolodets <mr.molodets@gmail.com>
2024-10-30 21:50:01 +0200

    configure nav bar

--- TMDBTestTask/TMDBTestTask/Modules/Main/View/MainViewController.swift
+++ TMDBTestTask/TMDBTestTask/Modules/Main/View/MainViewController.swift
@@ -11,15 +11,64 @@ import Combine
 class MainViewController: UIViewController {
     
     private var cancellables = Set<AnyCancellable>()
-    
     let viewModel = MainViewModel()
+        
+    private lazy var tableView: UITableView = {
+            let tableView = UITableView(frame: .zero, style: .plain)
+            tableView.showsVerticalScrollIndicator = false
+            tableView.backgroundColor = .clear
+            tableView.separatorStyle = .none
+            tableView.dataSource = self
+            tableView.delegate = self
+//            tableView.register(VisitedCountriesDetailTableViewCell.self, forCellReuseIdentifier: VisitedCountriesDetailTableViewCell.className)
+//            tableView.register(TravelsTableViewSectionHeader.self, forHeaderFooterViewReuseIdentifier: TravelsTableViewSectionHeader.className)
+    //        tableView.register(VisitedCountriesDetailTableViewSectionHeader.self, forHeaderFooterViewReuseIdentifier: VisitedCountriesDetailTableViewSectionHeader.className)
+            return tableView
+        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.onViewDidLoad()
         setupSubscriptions()
+        setupUI()
     }
     
+
+}
+
+
+private extension MainViewController {
+    func setupUI() {
+        setupNavigationBar()
+        view.backgroundColor = .white
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Popular Movies" // localize
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "arrow.up.and.down.text.horizontal")?.withTintColor(.gray), for: .normal)
+        button.frame = CGRectMake(0, 0, 44, 44)
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(sortedAction), for: .touchUpInside)
+        let barButtonItem = UIBarButtonItem(customView: button)
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.rightBarButtonItem?.tintColor = .gray
+        
+        let search = UISearchController(searchResultsController: nil)
+        search.delegate = self
+        search.searchBar.delegate = self
+        
+        navigationItem.searchController = search
+    }
+    
+ @objc private func sortedAction() {
+        
+    }
+}
+
+
+// MARK: - Subscriptions
+private extension MainViewController {
     func setupSubscriptions() {
         viewModel.viewState
             .receive(on: DispatchQueue.main)
@@ -62,3 +111,26 @@ class MainViewController: UIViewController {
     }
 }
 
+extension MainViewController: UISearchControllerDelegate {
+    
+}
+
+extension MainViewController: UISearchBarDelegate {
+    
+}
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+}
